15973b84a725abcd2c6afb51f0c63fb34a29160e
Anne L'Hôte <anne.lhote@gmail.com>
2018-11-20 14:19:53 +0000

    Add support for default value on translation_map

--- solr2es/solr2es.py
+++ solr2es/solr2es.py
@@ -140,7 +140,18 @@ def translate_doc(row, translation_map):
         return translated_key, translated_value
 
     d = tuple(translate(k, v) for k, v in row.items())
-    return tuples_to_dict(d)
+    return set_default_value(tuples_to_dict(d), translation_map)
+
+
+def set_default_value(d, translation_map):
+    default_values = list(map(lambda x: {x[0]: x[1]['default_value']},
+                              list(filter(lambda k: k[1] and type(k[1]) is dict and 'default_value' in k[1].keys(),
+                                          translation_map.items()))))
+    if len(default_values) > 0:
+        default_values[0].update(d)
+        return default_values[0]
+    else:
+        return d
 
 
 def retrieve_key_by_regexp(key, translation_map):
--- solr2es/test/test_migration.py
+++ solr2es/test/test_migration.py
@@ -136,10 +136,26 @@ class TestMigration(unittest.TestCase):
         doc = self.es.get_source(index='foo', doc_type=DEFAULT_ES_DOC_TYPE, id="142")
         self.assertEqual({'field1': 'content1', 'field2': 'content2'}, doc['nested'])
 
-    # def test_migrate_with_default_field_value(self):
+    def test_migrate_with_default_field_value_on_unexisting_field(self):
+        TestMigration.solr.add([{"id": "142"}])
+        self.solr2es.migrate('foo', '{"mappings": {"doc": {"properties": {"nested": {"type": "object"}}}}}',
+                             {"new_field": {'default_value': 'john doe'}, 'field1': {'name': 'field1'}})
+        doc = self.es.get_source(index='foo', doc_type=DEFAULT_ES_DOC_TYPE, id="142")
+        self.assertEqual('john doe', doc['new_field'])
+
+    # def test_migrate_with_default_field_value_on_existing_field(self):
+    #     requests.post('http://solr:8983/solr/test_core/schema', headers={'Content-Type': 'application/json'},
+    #                   data='{ "add-field":{ "name":"field1", "type":"string", "stored":true }}')
+    #     TestMigration.solr.add([{"id": "142", "field1": "content1"}])
+    #     self.solr2es.migrate('foo', '{"mappings": {"doc": {"properties": {"nested": {"type": "object"}}}}}',
+    #                          {"field1": {'default_value': 'content2'}})
+    #     doc = self.es.get_source(index='foo', doc_type=DEFAULT_ES_DOC_TYPE, id="142")
+    #     self.assertEqual('content1', doc['field1'])
+
+    # def test_migrate_with_default_field_value_on_multiple_fields(self):
     #     TestMigration.solr.add([{"id": "142"}])
     #     self.solr2es.migrate('foo', '{"mappings": {"doc": {"properties": {"nested": {"type": "object"}}}}}',
-    #                          {"new_field": {'default_value': 'john doe'}})
+    #                          {"new_field1": {'default_value': 'john doe'}, 'new_field2': {'default_value': 'adam martin'}})
     #     doc = self.es.get_source(index='foo', doc_type=DEFAULT_ES_DOC_TYPE, id="142")
     #     self.assertEqual('john doe', doc['new_field'])
 
