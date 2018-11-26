557e9e52ed4696b09530a8251ac4b1174b085f65
Bruno Thomas <bruno@barreverte.fr>
2018-11-26 15:30:41 +0000

    [refactor] rename

--- solr2es/__main__.py
+++ solr2es/__main__.py
@@ -116,18 +116,18 @@ class Solr2EsAsync(object):
                     cursor_ended = True
         LOGGER.info('processed %s documents', nb_results)
 
-    async def resume(self, postgresql_queue, index_name, mapping=None, translation_map=None):
+    async def resume(self, queue, index_name, mapping=None, translation_map=None):
         translation_dict = dict() if translation_map is None else translation_map
         if not await self.aes.indices.exists([index_name]):
             await self.aes.indices.create(index_name, body=mapping)
 
         nb_results = 0
-        results = await postgresql_queue.pop()
+        results = await queue.pop()
         while results:
             actions = create_es_actions(index_name, results, translation_dict)
             await self.aes.bulk(actions, index_name, DEFAULT_ES_DOC_TYPE, refresh=self.refresh)
             nb_results += len(results)
-            results = await postgresql_queue.pop()
+            results = await queue.pop()
         return nb_results
 
 
